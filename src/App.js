84abce89e6edadab24be38d081d7a6a1d4f4f156
j1999amar <j1999amar@gmail.com>
2022-06-18 11:59:56 +0530

    demo added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Message from './Message';
+
+function App() {
+  return (
+    <Message></Message>
+  );
+}
+
+export default App;
---
+++ src/Message.js
@@ -0,0 +1,4 @@
+function Message(){
+    return <h1>Hi!!! This is Amar</h1>
+}
+export default Message;
